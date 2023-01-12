2158de4f91043fc4d8fffe78ea25dd2e7538f423
Mohd <azeemsre1@gmail.com>
2023-01-12 17:38:17 +0530

    HomePageAdded

--- src/component/Pages/Home/Home.js
+++ src/component/Pages/Home/Home.js
@@ -1,10 +1,34 @@
 import React, { Fragment } from 'react'
+import './Home.css'
+import Tours from './Tours'
 
 
 const Home = () => {
   return (
     <Fragment>
-     
+    
+    <div className="latestAlbum">
+      <div className=" ">
+        <div className="album-para">
+          <p>Get our Latest Album</p>
+        </div>
+        <div className="main-playBtn">
+          <div className="playBtn">
+            <div class="play-button-outer">
+              <div class="play-button"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+      <div className="tours">TOURS</div>
+      <Tours />
+      <Tours />
+      <Tours />
+      <Tours />
+      <Tours />
+      <Tours />
+    
     </Fragment>
   )
 }
